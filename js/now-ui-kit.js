4a0d57c3f91192a8a1f1e25bead5873608fe3b78
Shehab Magdy El-Adl <cegres1@hotmail.com>
2026-04-06 19:57:24 +0200

    fix tab switching

--- js/now-ui-kit.js
+++ js/now-ui-kit.js
@@ -27,10 +27,10 @@ var navbar_initialized,
 
 $(document).ready(function() {
     //  Activate the Tooltips
-    $('[data-toggle="tooltip"], [rel="tooltip"]').tooltip();
+    $('[data-bs-toggle="tooltip"], [rel="tooltip"]').tooltip();
 
     // Activate Popovers and set color for popovers
-    $('[data-toggle="popover"]').each(function() {
+    $('[data-bs-toggle="popover"]').each(function() {
         color_class = $(this).data('color');
         $(this).popover({
             template: '<div class="popover popover-' + color_class + '" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
